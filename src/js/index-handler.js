e6a3069c238a98003e966812fa62550abc2db7d9
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-04-14 20:47:40 -0300

    Fix linter errors

--- src/js/index-handler.js
+++ src/js/index-handler.js
@@ -55,7 +55,7 @@ const IndexHandler = (() => {
       getFormCityName(),
       receiveWeatherData,
       receiveWeatherDataError,
-      getTemperatureTypeSelected()
+      getTemperatureTypeSelected(),
     );
   };
 
