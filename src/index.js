ca294e305d01a4844b75a333dba5d7c88609695c
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-04-14 12:05:27 -0300

    Create simple API call to the openweathermap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('meme');
+const getData = () => {
+  const apiKey = '67a1de75c70034e312753131c19bba85';
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`)
+    .then(getResolve)
+    .then(getResult);
+}
+
+const getResolve = (resolve) => {
+  return resolve.json();
+}
+
+const getResult = (result) => {
+  console.log(result);
+}
+
+getData();
