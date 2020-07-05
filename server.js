26052bd084db24ecdfa1839fdb81be6f1992c775
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-07-05 17:42:36 -0300

    Fix deployment

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+var path = require('path');
 var express = require('express');
+
 var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
